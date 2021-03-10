1d785cf35a5f687f0c9e0c91d2e3c48dd42e4f09
Abbos <abbos6264258@gmail.com>
2021-03-10 23:34:05 +0500

    feat: add state to 'Main' Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,38 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 
- class Header extends Component {
-  componentDidMount(){
-    this.Input.focus(); 
- }
+class Header extends Component {
+  componentDidMount() {
+    this.Input.focus();
+  }
 
-  handleEnter = (event) => {
-    if(event.key === 'Enter') 
-    this.search();
-  }  
-  handleClick = (e) => {
-    e.preventDefault();
-    this.search();
+  handleChange = (e) => { this.props.change(e.target.value); }
+  handleClick = (e) => {   e.preventDefault();  this.props.change(this.Input.value)  } 
+
+  render() {
+    return (
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <Link to="/" className="navbar-brand">Travel App</Link>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              ref={input => this.Input = input}
+              value={this.props.searchText}
+              autoComplete="off"
+              onChange={this.handleChange}></input>
+            <button
+              className="btn btn-secondary my-2 my-sm-0"
+              type="submit"
+              onClick={this.handleClick}
+              >Search</button>
+          </form>
+        </nav>
+      </header>
+    )
   }
-  search(){
-    console.log('work'); 
-  }
-      render() {
-        return (
-            <header>
-              <nav className="navbar navbar-dark bg-dark">
-                <Link to="/" className="navbar-brand">Travel App</Link>
-                <form className="form-inline my-2 my-lg-0">
-                  <input 
-                  className="form-control mr-sm-2" 
-                  type="text" 
-                  placeholder="Search" 
-                  ref={ (input) => {this.Input = input}} 
-                  autoComplete="off"
-                  onKeyDown={this.handleEnter}
-                  ></input>
-                  <button className="btn btn-secondary my-2 my-sm-0" type="submit" onClick={this.handleClick} >Search</button>
-                </form>
-              </nav>
-            </header>
-        )
-    }
 }
 
 export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,21 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export class Main extends Component {
+  constructor(){
+    super();
+    this.state = {
+      searchText: '', 
+    }
+  }
+  searchTextChange = (value) =>{
+    console.log(value)
+    this.setState({searchText: value})
+  }
+
   render() {
     return (
       <div>
-        <Header></Header>
+        <Header  searchText={this.state.searchText} change={this.searchTextChange}/>
         <h1>main page</h1>
       </div>
     )
