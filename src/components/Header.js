42ae83972da5a11451cf35bcea741fca9eaff6e7
Abbos <abbos6264258@gmail.com>
2021-03-10 12:00:43 +0500

    feat: add eventListener and autoFocus

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
  class Header extends Component {
+  componentDidMount(){
+    this.Input.focus(); 
+ }
+
+  handleEnter = (event) => {
+    if(event.key === 'Enter') 
+    this.search();
+  }  
+  handleClick = (e) => {
+    e.preventDefault();
+    this.search();
+  }
+  search(){
+    console.log('work'); 
+  }
       render() {
         return (
             <header>
               <nav className="navbar navbar-dark bg-dark">
                 <Link to="/" className="navbar-brand">Travel App</Link>
                 <form className="form-inline my-2 my-lg-0">
-                  <input className="form-control mr-sm-2" type="text" placeholder="Search"></input>
-                  <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                  <input 
+                  className="form-control mr-sm-2" 
+                  type="text" 
+                  placeholder="Search" 
+                  ref={ (input) => {this.Input = input}} 
+                  autoComplete="off"
+                  onKeyDown={this.handleEnter}
+                  ></input>
+                  <button className="btn btn-secondary my-2 my-sm-0" type="submit" onClick={this.handleClick} >Search</button>
                 </form>
               </nav>
             </header>
