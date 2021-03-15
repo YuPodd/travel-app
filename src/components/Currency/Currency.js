2283b3e771dff26b393aa1e4d18ada764a565467
Likvidas <50522742+Likvidas@users.noreply.github.com>
2021-03-15 21:41:41 +0300

    fix: currency

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -2,17 +2,14 @@ import React from 'react';
 import GetCurrency from '../../services/getCurrency';
 
 const Currency = ({money}) => {
-  // const money = 'RUB';
   const course = new GetCurrency();
   const [currencyUSD, setCurrencyUSD] = React.useState(0);
   const [currencyRUB, setCurrencyRUB] = React.useState(0);
-  // const [currencyEUR, setCurrencyEUR] = React.useState(0);
 
   React.useEffect(() => {
     course.getCurrency(money).then((data) => {
       setCurrencyUSD(data[0].rates[money].toFixed(2))
       setCurrencyRUB(data[1].rates[money].toFixed(2))
-      setCurrencyEUR(data[2].rates[money].toFixed(2))
     })
   })
 
@@ -20,7 +17,6 @@ const Currency = ({money}) => {
     <div className='widget-currency'>
       <p>USD : {currencyUSD}</p>
       <p>RUB : {currencyRUB}</p>
-      {/* <p>EUR : {currencyEUR}</p> */}
     </div>
   )
 }
